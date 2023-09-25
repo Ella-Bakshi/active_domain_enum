299fc9d7376acb13260d7163e9f2b599f6c3afa1
Anmol Bakshi <111650813+Ella-Bakshi@users.noreply.github.com>
2023-09-25 13:28:34 +0530

    Update domain_enum.sh

--- domain_enum.sh
+++ domain_enum.sh
@@ -10,17 +10,20 @@ if [ -z "$domain" ]; then
   exit 1
 fi
 
-# Run subfinder and httpx to get alive subdomains
-subfinder -d $domain -all -cs -silent | httpx -silent -title -wc -sc -cl -ct -location -web-server -asn -o ${domain}_alive.txt
+# Run subfinder to get subdomains, assuming that '-o' is the correct option for output file
+subfinder -d $domain -o ${domain}_subdomains.txt
+
+# Check the subdomains are alive using httpx, assuming that '--silent' is the correct option for silent mode
+cat ${domain}_subdomains.txt | httpx --silent -o ${domain}_alive.txt
 
 # Fetch historical domains from web.archive.org
-curl "http://web.archive.org/cdx/search/cdx?url=*.$domain/*&output=json&fl=original&collapse=urlkey" -s -k --insecure --path-as-is | sed 's/\["//g'| sed 's/"\],//g' | sort -u | tee -a ${domain}_archive.txt
+curl "http://web.archive.org/cdx/search/cdx?url=*.$domain/*&output=json&fl=original&collapse=urlkey" -s -k --insecure --path-as-is | jq -r '.[] | .[0]' | sort -u | tee -a ${domain}_archive.txt
 
 # Merge, sort, and get unique domains from both files and check if they are alive
-cat ${domain}_alive.txt ${domain}_archive.txt | sort -u | httpx -silent -o ${domain}_final.txt
+cat ${domain}_alive.txt ${domain}_archive.txt | sort -u | httpx --silent -o ${domain}_final.txt
 
 # Remove all other generated files
-rm -rf ${domain}_alive.txt ${domain}_archive.txt
+rm -rf ${domain}_alive.txt ${domain}_archive.txt ${domain}_subdomains.txt
 
 # Final message
 echo "All unique functioning alive links are saved in ${domain}_final.txt"
